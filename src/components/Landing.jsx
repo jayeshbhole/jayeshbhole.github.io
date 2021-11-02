20ca4ccc7303925dba83aef6addd6973b84cacf9
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-11-02 19:48:40 +0530

    Link Update

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,39 +3,40 @@ import { FiChevronDown } from "react-icons/fi";
 import "../scss/landing.scss";
 
 const Landing = () => {
-	return (
-		<section id="Landing">
-			<span>Hey there!</span>
-			<h1>
-				<span>I am</span> Jayesh Bhole
-			</h1>
-			<h2>
-				I am fond of creating stuff for the web!{" "}
-				<span style={{ fontSize: "2.3rem" }}>🕸️</span>
-			</h2>
-			<span style={{ display: "block" }}>
-				Developer (Self && Web), Designer,
-				<br />
-				Undergraduate @ IIIT Pune
-			</span>
-			<span id="cta">
-				<HashLink smooth to="/#Contact" className="link-button">
-					Contact!
-				</HashLink>
-				<a
-					target="blank"
-					href="https://drive.google.com/uc?export=download&id=1TnUvSZo7lijHOwmRKb4qYQxvkpMIm60U"
-					className="link-button">
-					Resume
-				</a>
-			</span>
+    return (
+        <section id='Landing'>
+            <span>Hey there!</span>
+            <h1>
+                <span>I am</span> Jayesh Bhole
+            </h1>
+            <h2>
+                I am fond of creating stuff for the web!{" "}
+                <span style={{ fontSize: "2.3rem" }}>🕸️</span>
+            </h2>
+            <span style={{ display: "block" }}>
+                Developer (Self && Web), Designer,
+                <br />
+                Undergraduate @ IIIT Pune
+            </span>
+            <span id='cta'>
+                <HashLink smooth to='/#Contact' className='link-button'>
+                    Contact!
+                </HashLink>
+                <a
+                    target='blank'
+                    rel='noopener noreferrer'
+                    href='https://drive.google.com/file/d/1WW7gzFVkDnnorkUw_1OuxpiQp0thjmvT/view?usp=sharing'
+                    className='link-button'>
+                    Resume
+                </a>
+            </span>
 
-			<HashLink smooth id="chevron" to="/#About">
-				<FiChevronDown />
-				<FiChevronDown />
-			</HashLink>
-		</section>
-	);
+            <HashLink smooth id='chevron' to='/#About'>
+                <FiChevronDown />
+                <FiChevronDown />
+            </HashLink>
+        </section>
+    );
 };
 
 export default Landing;
